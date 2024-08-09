1d5ba3330bcff8f4be4a78f127be8dfbaebd6e73
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-08-09 20:19:33 +0400

    plugin.js:changed supabase env vars names to original

--- config/plugins.js
+++ config/plugins.js
@@ -3,8 +3,8 @@ module.exports = ({env}) => ({
     config: {
       provider: 'strapi-provider-upload-supabase',
       providerOptions: {
-        apiUrl: env('SUPABASE_URL'),
-        apiKey: env('SUPABASE_KEY'),
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
         bucket: env('SUPABASE_BUCKET'),
         directory: env('SUPABASE_DIRECTORY'),
         options: {}
